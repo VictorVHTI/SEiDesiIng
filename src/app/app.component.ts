94f9d0eb14cbac40d52970561a70796f6390b32f
Victor Hugo Tellez Izquierdo <vhti_1992@hotmail.com>
2022-09-26 20:46:50 -0500

    finish login screen and add dashboard page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,24 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+
   title = 'solucionesEmpresariales';
+  screen: string = 'home';
+  page: string = 'dashboard';
+  sendEmail: boolean = false;
+  showLoader: boolean = false;
+
+  constructor() { }
+
+  changeScreen(screen: string) {
+    this.screen = screen;
+  }
+
+  login() {
+    this.showLoader = true;
+  }
+
+  register() {
+    this.showLoader = true;
+  }
 }
